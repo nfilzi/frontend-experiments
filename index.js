8406ec0a221cdc05f8d38fbab19d312da3c67cc8
Nicolas Filzi <nfilzi.webservices@gmail.com>
2020-05-07 18:39:39 +0200

    Add stimulus-flatpickr to test more things out
    Also add CSS to check how parcel 0 config works

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { Application } from "stimulus"
+import Flatpickr from 'stimulus-flatpickr'
+import 'flatpickr/dist/themes/dark.css'
+
 
 import HelloController from "./source/controllers/hello_controller"
 
 const application = Application.start()
 application.register("hello", HelloController)
+application.register('flatpickr', Flatpickr)
